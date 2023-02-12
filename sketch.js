d94d165953ab4dc2b795d12756197b549117d2f4
David Schmitz <97756932+Schmitz-David@users.noreply.github.com>
2023-02-12 17:48:37 +0100

    added color gradient

--- sketch.js
+++ sketch.js
@@ -3,6 +3,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   frameRate(2);
   positions=[0];
+  for(let i=0;i<300;i++) positions.push(0);
   initialize();
 }
 function initialize(){
@@ -17,9 +18,11 @@ function draw() {
   for(let d=0.25; d<1;d+=0.25)
     rect(width*(d-0.01),height*0.25,width*0.02, height*0.5);
   tmp=[0,0,0,-1];
-  for(let i=positions.length-1;i>=0;i--)
+  for(let i=positions.length-1;i>=0;i--){
+    fill(min(i,255),255*(1-i/positions.length),255);
     rect(width*0.25*(0.8+positions[i])-0.5*i*widthIncrement,height*0.75-
     (++tmp[positions[i]])*layerThickness,width*0.1+i*widthIncrement,layerThickness,layerThickness*0.05);
+  }
   for(let i=positions.length-1;i>=0&&tmp[3]==-1;i--) if(positions[i]!=2) tmp[3]=i;
   if(tmp[3]==-1) initialize(); else moveToNext(tmp[3],2);
 }
