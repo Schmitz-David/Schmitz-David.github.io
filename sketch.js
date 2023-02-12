d06b6c59e33acbd3ca6c9aaf7034b678754580da
David Schmitz <97756932+Schmitz-David@users.noreply.github.com>
2023-02-12 14:44:50 +0100

    testing Towers of Hanoi

--- sketch.js
+++ sketch.js
@@ -1,26 +1,34 @@
-let a;
-
+let positions, tmp, layerThickness, widthIncrement;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  pixelDensity(1);
-  stroke(255);
-  a = height / 2;
+  frameRate(2);
+  positions=[0];
+  initialize();
 }
-
+function initialize(){
+  for(let i=0;i<positions.length;i++) positions[i]=0;
+  positions.push(0);
+  layerThickness=height*0.5/positions.length;
+  widthIncrement=width*0.1/(positions.length-1);
+}
 function draw() {
-  if(mouseIsPressed) background(51);
-	else background(101);
-  line(0, a, width, a);
-  a = a - 0.5;
-  if (a < 0) {
-    a = height;
-  }
+  background(0);
+  fill(255);
+  for(let d=0.25; d<1;d+=0.25)
+    rect(width*(d-0.01),height*0.25,width*0.02, height*0.5);
+  tmp=[0,0,0,-1];
+  for(let i=positions.length-1;i>=0;i--)
+    rect(width*0.25*(0.8+positions[i])-0.5*i*widthIncrement,height*0.75-
+    (++tmp[positions[i]])*layerThickness,width*0.1+i*widthIncrement,layerThickness,layerThickness*0.05);
+  for(let i=positions.length-1;i>=0&&tmp[3]==-1;i--) if(positions[i]!=2) tmp[3]=i;
+  if(tmp[3]==-1) initialize(); else moveToNext(tmp[3],2);
 }
-
+function moveToNext(index, goal){
+  for(tmp[3]=index-1;positions[index]+goal+positions[tmp[3]]==3;tmp[3]--);
+  if(tmp[3]==-1) positions[index]=goal; else moveToNext(tmp[3],3-goal-positions[index]);
+}
 function windowResized(){
-	resizeCanvas(windowWidth, windowHeight);
-	a=height;
-}
-function deviceTurned(){
-	windowResized();
+  resizeCanvas(windowWidth, windowHeight);
+  layerThickness=height*0.5/positions.length;
+  widthIncrement=width*0.1/(positions.length-1);
 }
